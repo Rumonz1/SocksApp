443623f2923da39f9874bd1e3d5e56881d3a8deb
Rumonz <lone_assassin@mail.ru>
2023-05-12 01:44:47 +0300

    111

--- src/main/java/pro/sky/socksapp/Service/SocksService.java
+++ src/main/java/pro/sky/socksapp/Service/SocksService.java
@@ -5,11 +5,12 @@ import pro.sky.socksapp.Model.Socks;
 
 @Service
 public interface SocksService {
-    Socks editSocks(Socks socks, long quantity);
 
-    long getSocksByParameters(Socks.Color color, Socks.Size size, int cottonMin, int cottonMax);
-
-    boolean deleteSocks(Socks socks, long quantity);
-
-    Socks addSocks(Socks socks, long quantity);
+//    Socks editSocks(Socks socks, long quantity);
+//
+//    long getSocksByParameters(Socks.Color color, Socks.Size size, int cottonMin, int cottonMax);
+//
+//    boolean deleteSocks(Socks socks, long quantity);
+//
+//    Socks addSocks(Socks socks, long quantity);
 }
